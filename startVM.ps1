49701f237b1932706ad7b7f86f66ec23a846cf7f
Maurice Streubel <holzmolz@googlemail.com>
2013-08-21 20:37:43 +0200

    Only shows machines that are powered off

--- startVM.ps1
+++ startVM.ps1
@@ -1,10 +1,10 @@
-$Server = "1.2.3.4"
-$user = "YourAcount"
-$pass = "YourPass"
+$Server = "192.168.1.1"
+$user = "user"
+$pass = "pass"
 
 Connect-VIServer $Server -User $user -Password $pass
 
-$VMarray = Get-VM
+$VMarray = Get-VM  | where { ($_.PowerState -eq "PoweredOff") } 
 $index = 1
 
 Write-Host ""
@@ -14,7 +14,7 @@ $VMarray | Foreach {
     $index += 1
 }
 Write-Host ""
-Do { $GetMyNumber = Read-host "Write a number between 1-$($VM.length) to start a VM or 0 to abort"}
+Do { $GetMyNumber = Read-host "Write a number between 1-$($VMarray.length) to start a VM or 0 to abort"}
 until (($GetMyNumber -le $VMarray.length) -and ($GetMyNumber -ge 0))
 
 $index = 1
